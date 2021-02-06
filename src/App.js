acdbdbdd8adcbde7228f602c07e5d63d22e2fd66
gitcrackerUK <pfsiwek@gmail.com>
2021-02-06 08:52:10 +0000

    created Cards, Charts, CountryPicker components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Cards, Charts, CountryPicker } from 'components';
+import { GlobalStyle } from 'components/styled'
 
 function App() {
   return (
-    <></>
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Cards></Cards>
+      <Charts></Charts>
+      <CountryPicker></CountryPicker>
+    </>
   )
 }
 
