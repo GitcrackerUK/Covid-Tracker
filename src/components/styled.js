bfc2d830bd232df7ace16f7330aa6ffd8052044f
gitcrackerUK <pfsiwek@gmail.com>
2021-02-07 16:54:57 +0000

    global style

--- src/components/styled.js
+++ src/components/styled.js
@@ -2,6 +2,12 @@ import styled,{createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 body{
-  background-color:gray;
+
+  background-color:#f6f6f6;
 }
+`
+export const Container = styled.div`
+/* display:flex; */
+align-items:center;
+justify-content:center;
 `
