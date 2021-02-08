cb698cf070be2b2d17d6d658492362f195aaee78
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 23:33:07 +0000

    tidied  up

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { countries, sum, countryStatusTotalRoute, countryDayOneRoute } from 'api/routes'
-import React, { useEffect, useState } from 'react';
-import { Cards, Charts, CountryPicker } from 'components';
 import { GlobalStyle, Container } from 'components/styled'
-import { API } from 'api/index'
 import {Header, Main,Footer} from 'components';
-
-
-
-
+import React from 'react';
 
 export default function App() {
-  // let [res, setData] = useState()
-
-
-
-  useEffect(
-    () => {
-      // const getData = async () => {
-      //   try {
-      //     await API(sum)
-      //     setData( await API(sum))
-      //   } catch (err) {
-      //     console.log(err);
-      //     return err
-      //   }
-      // }
-      // getData()
-    }, []
-  )
-  
+ 
   return (
     <Container>
       <GlobalStyle></GlobalStyle>
