5e55cdbc39cf7a7ab081d6b479801934752a1338
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 08:29:41 +0000

    global styles for h1,h2,h3 and colors scheme

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,33 +1,57 @@
 import styled, { createGlobalStyle, css } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
-body{
-  background-color:#f6f6f6;
-}
-`
-export const Container = styled.div`
-width:1000px;
-margin:auto;
-height:100vw;
-`
+//colors------------
+export const Background = css`background-color:#f1f3f4;`
+export const CountryData = css`background-color:#F1F3F4;`
+export const GlobalBackground = css`background-color:#CCF2F4;`
+export const GlobalMain = css`background-color:#79BAC1;`
+export const SectionHeaders = css`background-color:#C7CED9;`
+export const Footer = css`background-color:#C7CED9;`
+//------------------
+
+//fonts------------------------------------------------------------------------------------------------------------------------------
 // weight -extra-light 200  
 //        -light 300
 //        -regular 400
 //        -medium 500
-const h1Light = css`
+export const h1Light = css`
 font-size:20px;
 font-weight:300;
 `
-const h1Regular = css`
+export const h1Regular = css`
 font-size:20px;
 font-weight:400;
 `
-const h2Regular = css`
+export const h2Light = css`
+font-size:18px;
+font-weight:300;
+`
+export const h2Regular = css`
 font-size:18px;
 font-weight:400;
 `
-
+export const h3Light = css`
+font-size:16px;
+font-weight:300;
+`
+export const h3Regular = css`
+font-size:16px;
+font-weight:400;
+`
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 export const shadow6 = css`box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.14), 0px 1px 18px rgba(0, 0, 0, 0.12), 0px 3px 5px rgba(0, 0, 0, 0.2);`
 export const shadow8 = css`box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px 14px rgba(0, 0, 0, 0.12), 0px 5px 5px rgba(0, 0, 0, 0.2);`
 export const shadow12 = css`box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);`
+export const GlobalStyle = createGlobalStyle`
+body{
+  margin:0px;
+  padding:0px;
+}
+`
+export const Container = styled.div`
+${Background};
+width:1000px;
+margin:auto;
+height:100vw;
+`
