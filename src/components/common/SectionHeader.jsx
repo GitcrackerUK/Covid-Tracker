cfaa62af1afff51a98ab9ee309eefc804c5cb45d
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 11:53:27 +0000

    added section header for main with rwd

--- src/components/common/SectionHeader.jsx
+++ src/components/common/SectionHeader.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
+import { SectionHeaders, h1Light, h1LightRES } from 'components/styled'
+import styled, { css } from 'styled-components'
 
 const Wrapper = styled.div`
+justify-content:center;
+align-items:center;
+border-radius:3px;
+${SectionHeaders};
+display:flex;
+${p => p.headerStyle ? css`
+    ${h1Light}
+    width:269px;
+    height:40px;
+    position:absolute;
+    top:40px;
+    left:40px;
+    @media(max-width:1000px){
+        top:4vw;
+        left:3vw;
+        width:27vw;
+        height:4.5vw;
+        ${h1LightRES}
+    }
+`: css``}
 `
 
-export default function SectonHeader(props) {
+export default function SectionHeader(props) {
     return (
         <Wrapper {...props}>
 
