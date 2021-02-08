23782514230fc013d0b9d1965b92085abd1e14f1
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 11:51:48 +0000

    changed font to Lato and added rwd font-size

--- src/components/styled.js
+++ src/components/styled.js
@@ -18,6 +18,10 @@ export const h1Light = css`
 font-size:20px;
 font-weight:300;
 `
+export const h1LightRES = css`
+font-size:2vw;
+font-weight:300;
+`
 export const h1Regular = css`
 font-size:20px;
 font-weight:400;
@@ -45,13 +49,18 @@ export const shadow8 = css`box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px
 export const shadow12 = css`box-shadow: 0px 12px 17px rgba(0, 0, 0, 0.14), 0px 5px 22px rgba(0, 0, 0, 0.12), 0px 7px 8px rgba(0, 0, 0, 0.2);`
 export const GlobalStyle = createGlobalStyle`
 body{
-  margin:0px;
+  margin:0px ;
   padding:0px;
+  font-family: 'Lato', sans-serif;
 }
 `
 export const Container = styled.div`
 ${Background};
 width:1000px;
-margin:auto;
-height:100vw;
+margin:0px auto;
+padding:1px;
+height:100vh;
+@media(max-width:1000px){
+  width:100%;
+}
 `
