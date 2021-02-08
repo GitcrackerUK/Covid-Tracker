0986d7eb64e32dcd15eb38dbe1ae5428b7b0c203
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 23:34:37 +0000

    Country list with scroll and onclick for each....

--- src/components/Main/CountryPicker.jsx
+++ src/components/Main/CountryPicker.jsx
@@ -1,7 +1,8 @@
-import SectionHeader from 'components/common/SectionHeader'
-import {shadow6} from 'components/styled'
-import styled from 'styled-components'
-import React from 'react'
+import SectionHeader from 'components/common/SectionHeader';
+import countries from 'store/countries.json'
+import { shadow6, h3Light, CountryData, GlobalMain } from 'components/styled';
+import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
 
 
 const Wrapper = styled.div`
@@ -9,6 +10,7 @@ margin:auto;
 width:90%;
 `
 const InnerWrapper = styled.div`
+overflow-y:auto;
 padding:1px;
 border-radius:2px;
 ${shadow6}
@@ -16,15 +18,68 @@ margin:auto;
 width:80%;
 background-color:#fff;
 height:500px;
+::-webkit-scrollbar {
+  width: 15px;
+  height:50px;
+};
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  margin-top: 30px;
+  border-radius:10px;
+};
+::-webkit-scrollbar-thumb {
+    background:#cecece;
+    border-radius:10px;
+    ${shadow6};
+  
+}
+`
+const UL = styled.ul`
+margin:10px 0px;
+padding-inline-start:10px;
+`
+const LI = styled.li`
+cursor: pointer;
+width:90%;
+list-style-type:none;
+border-bottom:1px solid #fff;
+${h3Light};
+
+:hover{
+    border-bottom:1px solid #79BAC1;
+    ${CountryData};
+}
+:active{
+    ${GlobalMain};
+}
 `
 
 
-export default function CountryPicker(){
-    return(
+export default function CountryPicker(props) {
+    const [data, setData] = useState([])
+
+
+    useEffect(() => {
+        const data = () => {
+            setData(countries.result)
+        }
+        data()
+    }, [])
+    
+    function handleClick(e){
+        console.log(e.target);
+    }
+
+    return (
         <Wrapper>
-           
+
             <InnerWrapper>
-            <SectionHeader Regular >Countries</SectionHeader>
+                <SectionHeader Regular >Countries</SectionHeader>
+                <UL>
+                    {data.map((item) => {
+                        return <LI onClick={(e)=>props.handleClick(e)}>{item.country}</LI>
+                    })}
+                </UL>
 
             </InnerWrapper>
 
