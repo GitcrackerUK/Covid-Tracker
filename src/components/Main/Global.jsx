5b61026d52df25cb235c7980f7eb0af2da4cb498
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 13:42:39 +0000

    created Global component for Main component

---
+++ src/components/Main/Global.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
+
+
+
+export default function Global(){
+    return(
+        <Wrapper>
+            <h1>global</h1>
+        </Wrapper>
+    )
+}
