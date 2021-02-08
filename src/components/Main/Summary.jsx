4528f7eeb93f38cb55c3174d923f2c3a081c3988
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 13:44:38 +0000

    created Summary component

---
+++ src/components/Main/Summary.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+
+`
+
+
+
+export default function Summary(){
+    return(
+        <Wrapper>
+            <h1>summary</h1>
+        </Wrapper>
+    )
+}
