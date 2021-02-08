7d4d3d8fe9f2cd2a778596702c7923b7603122cb
gitcrackerUK <pfsiwek@gmail.com>
2021-02-08 11:54:04 +0000

    added covid.png to  header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,30 @@
 import SectionHeader from 'components/common/SectionHeader'
 import Covid from 'components/common/Covid.jsx'
-import styled from 'styled-components'
+import styled,{css} from 'styled-components'
+import { covidMain } from 'img'
 import React from 'react'
 
-
 const Wrapper = styled.div`
+position:relative;
 height:135px;
-width:100vw;
+width:100%;
 `
 const Image = styled.div`
-
+background-image:url(${covidMain});
+background-repeat: no-repeat;
+margin:40px 5vw 0 auto;
+background-size: 100% ;
+height:20vw;
+width:80%;
 `
 
 
 export default function Header() {
     return (
         <Wrapper>
-           <SectionHeader>Header</SectionHeader>
-           <Image></Image>
-           <Covid></Covid>
-
-            
+            <SectionHeader headerStyle >Covid-19 Data Explorer</SectionHeader>
+            <Image></Image>
+            {/* <Covid size={30}></Covid> */}
         </Wrapper>
     )
 }
