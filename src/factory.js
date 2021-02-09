3d4bf41972c5bea5517a834f9dbc70e9fa8e5c7e
gitcrackerUK <pfsiwek@gmail.com>
2021-02-09 23:36:46 +0000

    removeComaPlus

--- src/factory.js
+++ src/factory.js
@@ -1,3 +1,6 @@
 export const removeComa = (data)=>{
     return parseFloat(data.replace(/,/g, ''))
 }
+export const removeComaPlus = (data)=>{
+    return data?parseFloat(data.replace(/[,+]/g, '')):null
+}
